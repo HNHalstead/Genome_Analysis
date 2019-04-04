bc9d62cdbe3a0b381ed551e646a114d7ba40fccb
HNHalstead <47102761+HNHalstead@users.noreply.github.com>
2019-04-04 22:42:50 +0200

    Update sbatch_script_illumina_trim.sh
    change of rutime

--- Illumina_trim/sbatch_script_illumina_trim.sh
+++ Illumina_trim/sbatch_script_illumina_trim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #SBATCH -A g2019003
 #SBATCH -p core
-#SBATCH -n 2
-#SBATCH -t 01:00:00
+#SBATCH -n 1
+#SBATCH -t 02:00:00
 #SBATCH -J hh_trim_illumina_efaecium
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user halsteadholly73@gmail.com
