fba45a18384420d2ba269c9725703f98cd7eba90
HNHalstead <47102761+HNHalstead@users.noreply.github.com>
2019-04-04 22:08:08 +0200

    Update sbatch_script_illumina_trim.sh

--- Illumina_trim/sbatch_script_illumina_trim.sh
+++ Illumina_trim/sbatch_script_illumina_trim.sh
@@ -15,4 +15,4 @@ module load trimmomatic
 # Your commands
 
 #Settings for trimmming
-java -jar trimmomatic-0.35.jar PE -phred33 E745-1.L500_SZAXPI015146-56_1_clean.fq.gz E745-1.L500_SZAXPI015146-56_2_clean.fq.gz E745-1.L500_SZAXPI015146-56_clean_trim_f_paired.fq.gz E745-1.L500_SZAXPI015146-56_clean_trim_f_unpaired.fq.gz E745-1.L500_SZAXPI015146-56_clean_trim_r_paired.fq.gz E745-1.L500_SZAXPI015146-56_clean_trim_r_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:27 MINLEN:36
+java -jar trimmomatic-0.36.jar PE -phred33 E745-1.L500_SZAXPI015146-56_1_clean.fq.gz E745-1.L500_SZAXPI015146-56_2_clean.fq.gz E745-1.L500_SZAXPI015146-56_clean_trim_f_paired.fq.gz E745-1.L500_SZAXPI015146-56_clean_trim_f_unpaired.fq.gz E745-1.L500_SZAXPI015146-56_clean_trim_r_paired.fq.gz E745-1.L500_SZAXPI015146-56_clean_trim_r_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:27 MINLEN:36
