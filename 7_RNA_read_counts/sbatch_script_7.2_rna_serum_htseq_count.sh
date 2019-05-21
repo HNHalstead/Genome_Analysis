0242bacea3c1623d2d4143d2c917e51e0730535b
HNHalstead <47102761+HNHalstead@users.noreply.github.com>
2019-05-21 14:32:52 +0200

    Update sbatch_script_7.2_rna_serum_htseq_count.sh

--- 7_RNA_read_counts/sbatch_script_7.2_rna_serum_htseq_count.sh
+++ 7_RNA_read_counts/sbatch_script_7.2_rna_serum_htseq_count.sh
@@ -16,8 +16,6 @@ module load samtools/1.9
 module load htseq/0.9.1
 
 samtools view output_rna_serum_map_ERR1797969.bam | htseq-count --format bam --type CDS  output_rna_serum_map_ERR1797969.bam /home/hoha8653/GenomeAnalysis/genome_soft/map_illu_pac/combined_alignment/prokka_combined_align/e_faecium_combined_align_annot.gtf > rna_serum_ERR1797969_counts.txt
-#Option --order pos is used for htseq-count as bam was ordered by pos
-
 
 
 module load bioinfo-tools
@@ -25,6 +23,7 @@ module load samtools/1.9
 module load htseq/0.9.1
 
 samtools view output_rna_serum_map_ERR1797970.bam | htseq-count --format bam --type CDS output_rna_serum_map_ERR1797970.bam /home/hoha8653/GenomeAnalysis/genome_soft/map_illu_pac/combined_alignment/prokka_combined_align/e_faecium_combined_align_annot.gtf > rna_serum_ERR1797970_counts.txt
+
 
 module load bioinfo-tools
 module load samtools/1.9
